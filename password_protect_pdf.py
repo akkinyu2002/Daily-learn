dbbb5d19ee46000555218f6945a3b7a04db724f8
Aakash Neupane <neupaneakash687@gmail.com>
2026-03-13 22:08:10 +0545

    feat: setup basic structure and imports for pdf protector

---
+++ password_protect_pdf.py
@@ -0,0 +1,13 @@
+from pypdf import PdfReader, PdfWriter
+import sys
+import os
+
+def protect_pdf(input_pdf_path, output_pdf_path, password):
+    """
+    Adds a password to a PDF file.
+    
+    :param input_pdf_path: Path to the original PDF file
+    :param output_pdf_path: Path where the protected PDF will be saved
+    :param password: Password to apply to the PDF
+    """
+    pass
