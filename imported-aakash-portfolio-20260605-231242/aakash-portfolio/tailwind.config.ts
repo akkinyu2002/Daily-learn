bfc47b624bcb0875a9d73d75154b1ce1e653d1d8
Aakash Neupane <neupaneakash687@gmail.com>
2026-06-05 23:20:31 +0545

    Add imported-aakash-portfolio-20260605-231242/aakash-portfolio/tailwind.config.ts

---
+++ imported-aakash-portfolio-20260605-231242/aakash-portfolio/tailwind.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/app/**/*.{ts,tsx}',
+    './src/components/**/*.{ts,tsx}',
+    './src/pages/**/*.{ts,tsx}',
+  ],
+};
