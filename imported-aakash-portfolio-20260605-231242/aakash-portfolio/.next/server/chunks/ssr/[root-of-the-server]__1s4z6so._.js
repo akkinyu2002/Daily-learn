df6349216021b77b864dcf79d0868aac41624771
Aakash Neupane <neupaneakash687@gmail.com>
2026-06-05 23:16:36 +0545

    Add imported-aakash-portfolio-20260605-231242\aakash-portfolio\.next\server\chunks\ssr\[root-of-the-server]__1s4z6so._.js

---
+++ imported-aakash-portfolio-20260605-231242/aakash-portfolio/.next/server/chunks/ssr/[root-of-the-server]__1s4z6so._.js
@@ -0,0 +1,3 @@
+module.exports=[93695,(a,b,c)=>{b.exports=a.x("next/dist/shared/lib/no-fallback-error.external.js",()=>require("next/dist/shared/lib/no-fallback-error.external.js"))},10585,a=>{a.v("/_next/static/media/favicon.2vob68tjqpejf.ico"+(globalThis.NEXT_CLIENT_ASSET_SUFFIX||""))},68611,a=>{"use strict";let b={src:a.i(10585).default,width:256,height:256};a.s(["default",0,b])},41587,a=>{"use strict";var b=a.i(7997);a.s(["default",0,function(){return(0,b.jsxs)("div",{className:"mx-auto max-w-6xl px-6 py-20",children:[(0,b.jsx)("h1",{className:"text-3xl font-bold",children:"Services"}),(0,b.jsx)("p",{className:"mt-4 text-zinc-600",children:"Detailed service pages coming soon."})]})}])},33772,a=>{a.n(a.i(41587))}];
+
+//# sourceMappingURL=%5Broot-of-the-server%5D__1s4z6so._.js.map
