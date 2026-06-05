0642a79804aeb6f8893105fe55467b801a21d38e
Aakash Neupane <neupaneakash687@gmail.com>
2026-06-05 23:20:28 +0545

    Add imported-aakash-portfolio-20260605-231242/aakash-portfolio/src/app/page.tsx

---
+++ imported-aakash-portfolio-20260605-231242/aakash-portfolio/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "../components/home/hero";
+import FeaturedWork from "../components/home/featured-work";
+import CtaSection from "../components/home/cta-section";
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      <FeaturedWork />
+      <CtaSection />
+    </div>
+  );
+}
