2c4bfe6c481bebee65a41d21af5be5cf16db5710
Aakash Neupane <neupaneakash687@gmail.com>
2026-06-05 23:15:56 +0545

    Add imported-aakash-portfolio-20260605-231242\aakash-portfolio\.next\dev\static\chunks\src_app_layout_tsx_007e4b2._.js

---
+++ imported-aakash-portfolio-20260605-231242/aakash-portfolio/.next/dev/static/chunks/src_app_layout_tsx_007e4b2._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/[root-of-the-server]__06oau27._.css","static/chunks/node_modules_next_dist_0ezrxgu._.js","static/chunks/src_components_layout_scroll-progress_tsx_1rgfr_r._.js"],
+    source: "dynamic"
+});
